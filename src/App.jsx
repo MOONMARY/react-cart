0d47a8fb94bba2a96e8a7cc1c70c29048b1e5484
MOONMARY <firstmoon5@naver.com>
2025-02-11 16:59:23 +0900

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,43 @@
 import { useState, useEffect } from "react";
-import CartHeader from "./components/CartHeader.jsx";
-import ShopList from "./components/ShoptList.jsx";
-import CartInput from "./components/CartInput.jsx";
-import BoughtList from "./components/BoughtList.jsx";
-import CartFooter from "./components/CartFooter.jsx";
+
+import CartHeader from "./components/CartHeader";
+import ShopList from "./components/ShopList";
+import CartInput from "./components/CartInput";
+import BoughtList from "./components/BoughtList";
+import CartFooter from "./components/CartFooter";
 
 function App() {
-  const apiUrl = "http://localhost:3000/shoplist";
-  // //서버로부터 API 호출해서 쇼핑 목록 받아오기
+  // const apiUrl = "http://localhost:1337/shoplist";
+  // const apiUrl = "http://localhost:3000/shoplist";
+  // const apiUrl = "http://localhost:8088/api/shoplist";
+
+  // .env로부터 환경 변수를 불러올 때
+  // - CREATE-REACT-APP에서는 process.env.REACT_APP_환경변수
+  // - VITE에서는 import.meta.env.VITE_환경변수를 읽을 수 있다
+  const apiUrl = `http://${import.meta.env.VITE_API_HOST}:18088/api/shoplist`;
+
+  //  서버로부터 API 호출해서 쇼핑 목록 받아오기
   // const [itemList, setItemList] = useState([
-  //   //상태변수(itemList), 상태변경함수(setItemList)에 할당된다
-  //   { id: 1, name: "새우깡", isBought: false },
-  //   { id: 2, name: "포키", isBought: false },
-  //   { id: 3, name: "고래밥", isBought: true },
-  //   { id: 4, name: "콘칩", isBought: false },
+  //   { id: 1, name: "무", isBought: false },
+  //   { id: 2, name: "배추", isBought: false },
+  //   { id: 3, name: "쪽파", isBought: true },
+  //   { id: 4, name: "고춧가루", isBought: false },
   // ]);
   const [itemList, setItemList] = useState([]);
-  //산 물건 보기 여부를 체크할 state
+  //  산 물건 보기 여부를 체크할 state
   const [showBoughtItems, setShowBoughtItems] = useState(true);
-
-  //페이지 로딩상태 체크 state
+  //  페이지 로딩 상태 체크 state
   const [isLoading, setIsLoading] = useState(true);
-  //에러 메시지 출력을 위한 state
+  //  에러 메시지 출력을 위한 state
   const [error, setError] = useState(null);
 
-  //isBought === false 인 것만 필터링
+  //  isBought === false인 것만 필터링
+  //  isBought === false인 것들
   const shopItems = itemList.filter((item) => !item.isBought);
-
-  //isBought === true 인 것만 필터링
+  //  isBought === true인 것들의 목록
   const boughtItems = itemList.filter((item) => item.isBought);
 
-  //API에서 목록 받아오는 함수
+  //  API에서 목록 받아오는 함수
   const fetchItems = async () => {
     try {
       const response = await fetch(apiUrl);
@@ -40,35 +47,36 @@ function App() {
       const data = await response.json();
       // console.log(data);
       setItemList(data);
-      setIsLoading(false); //로딩이 끝났음을 알림
+      setIsLoading(false); //  로딩이 끝났음을 알림
     } catch (err) {
       // console.error(err);
       setError(err.message);
-      setIsLoading(false); //로딩이 끝남
+      setIsLoading(false); //  로딩이 끝남
     }
   };
+
   useEffect(() => {
     fetchItems();
-  }, []); //컴포넌트가 처음 로딩되었을 떄의 이펙트 발생
+  }, []); //  -> 컴포넌트가 처음 로딩되었을 때의 이펙트 발생
 
   if (isLoading) return <div>로딩 중...</div>;
   if (error) return <div>에러: {error}</div>;
 
-  //새 아이템 추가
+  //  새 아이템 추가
   const addNewItem = async (name) => {
-    //id 생성 -> id의 최대값 + 1
+    //  id 생성 -> id의 최댓값 + 1
     const newId =
       itemList.length > 0
         ? Math.max(...itemList.map((item) => item.id)) + 1
         : 1;
-    //객체 생성
-    //속성이 key이름과 값 이름이 같을 때 -> 줄여쓸 수 있다
-    //name: name => name
+    //  객체 생성
+    //  속성이 key이름과 값 이름이 같을 때 -> 줄여쓸 수 있다.
+    //  name: name => name
     const newItem = { id: newId, name, isBought: false };
-    //itemList에 새 아이템 추가
-    // const newItemList = [...itemList, newItem];
+    //  itemList에 새 아이템 추가
+    // const newItemList = [...itemL
 
-    //-> REST 서버에 POST 호출 -> CREATE
+    //  -> REST 서버에 POST 호출 -> CREATE
     try {
       const response = await fetch(apiUrl, {
         method: "POST",
@@ -77,27 +85,31 @@ function App() {
         },
         body: JSON.stringify(newItem),
       });
-      //요청 결과 확인
+      //  요청 결과 확인
       if (!response.ok) {
         throw new Error("새 아이템을 추가하지 못했습니다.");
       }
-      //리스트 갱신
+      //  리스트 갱신
       fetchItems();
     } catch (err) {
       setError(err.message);
     }
   };
-  // id == isBought를 true <-> false
+
+  //  id => isBought를 true <-> false
   const toggleBought = async (id) => {
-    // const newItemList = itemList.map((item) =>
-    //   item.id === id ? { ...item, isBought: !item.isBought } : item
-    // );
-    // setItemList(newItemList);
-    //id로 아이템을 찾아서
-    //해당 아이템의 isBought값을 반전 true <-> false
+    /*
+    const newItemList = itemList.map((item) =>
+      item.id === id ? { ...item, isBought: !item.isBought } 
+                        : item
+    );
+    setItemList(newItemList);
+    */
+    //  id로 아이템을 찾아서
+    //  해당 아이템의 isBought 값을 반전 true <-> false
     const updatedItem = itemList.find((item) => item.id === id);
     updatedItem.isBought = !updatedItem.isBought;
-    //서버에 UPDATE 요청 전송
+    //  서버에 UPDATE 요청 전송
 
     try {
       const response = await fetch(`${apiUrl}/${id}`, {
@@ -117,11 +129,11 @@ function App() {
     }
   };
 
-  // id => item 삭제
+  //  id => item 삭제
   const deleteItem = async (id) => {
     // const newItemList = itemList.filter((item) => item.id !== id);
     // setItemList(newItemList);
-    //DELETE method로 요청
+    //  DELETE method로 요청
     try {
       const response = await fetch(`${apiUrl}/${id}`, {
         method: "DELETE",
@@ -129,13 +141,14 @@ function App() {
       if (!response.ok) {
         throw new Error("아이템을 삭제하지 못했습니다.");
       }
-      //목록 갱신
+      //  목록 갱신
       fetchItems();
     } catch (err) {
       console.error(err);
       setError(err.message);
     }
   };
+
   return (
     <div>
       <CartHeader />
